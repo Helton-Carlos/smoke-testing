8d10345d9f5d9de71bf262d4851c68b5e7f845c8
Helton-Carlos <heltoncarlospa@hotmail.com>
2022-10-09 21:43:19 -0300

    fix: key product

--- src/components/STcard/STcard.vue
+++ src/components/STcard/STcard.vue
@@ -17,7 +17,7 @@ const produtos = ref<produtos[]>([
 <template>
   <div class="bg-white">  
       <div class="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-        <div class="group relative" v-for="product in produtos" :key="product">
+        <div class="group relative" v-for="product in produtos" :key="product.produto">
           <div
             class="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
             <img src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg"
