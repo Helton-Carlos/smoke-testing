f86ed57729cdc6b7b644c53f4d0c55a737519e03
Helton-Carlos <heltoncarlospa@hotmail.com>
2022-10-12 23:58:39 -0300

    feat: add emit in input

--- src/components/STinput/STInput.vue
+++ src/components/STinput/STInput.vue
@@ -2,18 +2,14 @@
 import { ref } from 'vue';
 
 const inputs = ref<string>("");
-
-function pesquisar(){
-  console.log(inputs.value)
-}
 </script>
 
 <template>
   <div class="my-4 flex">
     <input type="text" id="first_name"
       class="block w-full px-4 py-1 text-indico-700 bg-white border rounded-md focus:border-indico-400 focus:ring-indico-300 focus:outline-none focus:ring focus:ring-opacity-40"
-      placeholder="Estou procurando?" v-model="inputs" @keyup.enter="pesquisar()" required>
+      placeholder="Estou procurando?" v-model="inputs" @keyup.enter="$emit('pesquisar', inputs)" required>
    
-    <button class="mx-2 bg-transparent hover:bg-sky-700 text-sky-700 font-semibold hover:text-white py-2 px-4 border border-sky-700 hover:border-transparent rounded" @click="pesquisar()">Buscar</button>
+    <button class="mx-2 bg-transparent hover:bg-sky-700 text-sky-700 font-semibold hover:text-white py-2 px-4 border border-sky-700 hover:border-transparent rounded" @click="$emit('pesquisar', inputs)">Buscar</button>
   </div>
 </template>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
 import STinput from '../components/STinput/STinput.vue';
 import STcard from '../components/STcard/STcard.vue';
+
+function buscar(result: string) {
+  console.log(result);
+}
 </script>
 
 <template>
   <div>
-    <STinput />
+    <STinput @pesquisar="buscar" />
     <div class="my-4">
       <STcard />
     </div>
